90c845a1f30668eb31dbba7483ed9caa04c862cb
nscharfe <nscharfe@gmail.com>
2017-11-08 13:28:47 -0800

    feat(mock_helper): add mock helper

---
+++ lib/task_bunny/test_helpers/mock_helper.ex
@@ -0,0 +1,58 @@
+defmodule TaskBunny.TestHelpers.MockHelper do
+  @moduledoc """
+  Module to mock TaskBunny during testing.
+  Adapted from https://github.com/shinyscorpion/task_bunny/issues/44
+
+  On setup function, call:
+
+  ```
+  MockHelper.mock_publish()
+  on_exit(&:meck.unload/0)
+  ```
+  """
+  alias TaskBunny.{Publisher, Queue, Message}
+
+  @doc """
+  Mocks TaskBunny.Queue.Publisher so that we can isolate our tests from RabbitMQ.
+  """
+  def mock_publish do
+    :meck.expect Publisher, :publish!, fn (_host, _queue, _message) ->
+      :ok
+    end
+    :meck.expect Publisher, :publish!, fn (_host, _queue, _message, _options) ->
+      :ok
+    end
+    :meck.expect Queue, :declare_with_subqueues, fn (_host, _queue) ->
+      :ok
+    end
+  end
+
+  @doc """
+  Mocks TaskBunny.Publisher and performs jobs given immediately.
+
+  Once you call `MockHelper.sync_publish()`, `TaskBunny.Publisher` will perform the job immediately instead of sending a message to queue.
+  """
+  def sync_publish do
+    :meck.expect Publisher, :publish!, fn (_host, _queue, message, _option) ->
+      {:ok, json} = Message.decode(message)
+      json["job"].perform(json["payload"])
+    end
+  end
+
+  @doc """
+  Check if the job is enqueued with given condition
+  """
+  def enqueued?(job, payload \\ nil) do
+    history = :meck.history(Publisher)
+
+    queued = Enum.find history, fn ({_pid, {_module, :publish!, args}, _ret}) ->
+      case args do
+        [_h, _q, message | _] ->
+          {:ok, json} = Message.decode(message)
+          json["job"] == job && (is_nil(payload) || json["payload"] == payload)
+        _ -> false
+      end
+    end
+    queued != nil
+  end
+end
